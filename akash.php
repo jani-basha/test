f90e79b70aee3360d4faf9e78ba938e69bbd876f
jani.basha <jani.basha@evergent.com>
2018-05-02 19:33:27 +0530

    merged with akash.php with akash branch

--- akash.php
+++ akash.php
@@ -1,13 +1,17 @@
 <?php
-class akash{
-    public function add($x,$y){
-        echo 'total is:' ($x+$y);
-    } 
+
+class akash {
+
+    public function add($x, $y) {
+        echo 'total is:'($x + $y);
+    }
+
+    public function subtract($x, $y) {
+        echo 'Subtotal is:' . ($x - $y);
+    }
+
 }
-    public function subtract($x,$y){
-        echo 'Subtotal is:'.($x-$y);
-    }    
 
 $a = new akash();
-$a->add(20,30);
-$a->subtract(30,12);
+$a->add(20, 30);
+$a->subtract(30, 12);
