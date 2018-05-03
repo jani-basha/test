56b3cb0fd920a90a89342a2527f2241a174b59b3
jani.basha <jani.basha@evergent.com>
2018-05-03 12:29:27 +0530

    multiply function added in aksh.php in akash branch

--- akash.php
+++ akash.php
@@ -9,7 +9,11 @@ class akash {
     public function subtract($x, $y) {
         echo 'Subtotal is:' . ($x - $y);
     }
+    public function multiply($x,$y){
+        echo 'Multiply totl is:'.($x*$y);
+    }
 }
 $a = new akash();
 $a->add(20, 30);
 $a->subtract(30, 12);
+$a->multiply(15,45);
